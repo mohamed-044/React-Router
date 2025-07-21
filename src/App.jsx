eef2db1b723f7b5c69e39239d619fe3557b48f2e
mohamed-044 <mohamed.boulatika04@gmail.com>
2025-07-21 10:24:06 +0200

    add route for home page

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,14 @@
+import { Route, Routes } from "react-router";
 import './App.css'
+import Home from "./pages/Home.jsx"
 
 function App() {
 
 
   return (
-    <>
-
-    </>
+    <Routes>
+      <Route path="/" element={<Home />} />
+    </Routes>
   )
 }
 
