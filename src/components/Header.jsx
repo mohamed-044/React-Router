dc4cb8073738c5f3b09aafb3491483e639c3b533
mohamed-044 <mohamed.boulatika04@gmail.com>
2025-07-21 13:59:21 +0200

    add navbar with home, vetements, chaussures

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,4 +1,17 @@
+import Nav from 'react-bootstrap/Nav';
 function Header() {
-  return <p>Header</p>;
+  return (
+    <Nav className="justify-content-end" activeKey="/">
+      <Nav.Item as="li">
+        <Nav.Link className="text-dark" href="/">Accueil</Nav.Link>
+      </Nav.Item>
+      <Nav.Item as="li">
+        <Nav.Link className="text-dark" href="/category/vetements">Vêtements</Nav.Link>
+      </Nav.Item>
+      <Nav.Item as="li">
+        <Nav.Link className="text-dark" href="/category/chaussures">Chaussures</Nav.Link>
+      </Nav.Item>
+    </Nav>
+  );;
 }
 export default Header;
