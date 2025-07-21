6fed8be26604c4d1701a36c399af7695e656720a
mohamed-044 <mohamed.boulatika04@gmail.com>
2025-07-21 10:59:05 +0200

    use layout

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 import { Route, Routes } from "react-router";
 import "./App.css";
-import Home from "./pages/Home.jsx";
+import Layout from "./components/Layout.jsx";
 
 function App() {
   return (
     <Routes>
-      <Route path="/" element={<Home />} />
+      <Route element={<Layout />}>
+        <Route path="/" />
+      </Route>
     </Routes>
   );
 }
