e63cb9a5017a6c9a5c1d41353f20b2b3bb0c1973
mohamed-044 <mohamed.boulatika04@gmail.com>
2025-07-21 14:21:38 +0200

    add as container on main

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,22 @@
-import { Route, Routes, Link } from "react-router";
+import { Route, Routes } from "react-router";
+import { Container } from "react-bootstrap";
 import "./App.css";
-import Home from "./pages/Home.jsx"
+import Home from "./pages/Home.jsx";
 import Layout from "./components/Layout.jsx";
 import Category from "./pages/Category.jsx";
 import NotFound from "./pages/NotFound.jsx";
 
 function App() {
   return (
+    <Container as="main">
     <Routes>
       <Route element={<Layout />}>
-        <Route path="/" element={<Home />}/>
+        <Route path="/" element={<Home />} />
         <Route path="category/:categorie" element={<Category />} />
         <Route path="*" element={<NotFound />} />
       </Route>
     </Routes>
+    </Container>
   );
 }
 
