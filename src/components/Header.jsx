8238028ed4b51d977a359060f9ccfab4534c96c7
mohamed-044 <mohamed.boulatika04@gmail.com>
2025-07-21 14:38:54 +0200

    use Link component

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,15 +1,16 @@
 import Nav from 'react-bootstrap/Nav';
+import { Link } from 'react-router';
 function Header() {
   return (
     <Nav className="justify-content-end" activeKey="/">
-      <Nav.Item as="li">
-        <Nav.Link className="text-dark" href="/">Accueil</Nav.Link>
+      <Nav.Item>
+        <Nav.Link className="text-dark" as={Link} to="/">Accueil</Nav.Link>
       </Nav.Item>
-      <Nav.Item as="li">
-        <Nav.Link className="text-dark" href="/category/vetements">Vêtements</Nav.Link>
+      <Nav.Item>
+        <Nav.Link className="text-dark" as={Link} to="/category/vetements">Vêtements</Nav.Link>
       </Nav.Item>
-      <Nav.Item as="li">
-        <Nav.Link className="text-dark" href="/category/chaussures">Chaussures</Nav.Link>
+      <Nav.Item>
+        <Nav.Link className="text-dark" as={Link} to="/category/chaussures">Chaussures</Nav.Link>
       </Nav.Item>
     </Nav>
   );;
