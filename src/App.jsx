895934c1598479d83752565a2a368589182971d6
mohamed-044 <mohamed.boulatika04@gmail.com>
2025-07-21 11:22:14 +0200

    show category

--- src/App.jsx
+++ src/App.jsx
@@ -2,12 +2,14 @@ import { Route, Routes } from "react-router";
 import "./App.css";
 import Home from "./pages/Home.jsx"
 import Layout from "./components/Layout.jsx";
+import Category from "./components/Category.jsx";
 
 function App() {
   return (
     <Routes>
       <Route element={<Layout />}>
         <Route path="/" element={<Home />}/>
+        <Route path="category/:categorie" element={<Category />} />
       </Route>
     </Routes>
   );
