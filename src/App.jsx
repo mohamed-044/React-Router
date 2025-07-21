f8b0f760e783fc30f3b8fe57e3acbd232eaf3c9d
mohamed-044 <mohamed.boulatika04@gmail.com>
2025-07-21 11:55:56 +0200

    add NotFound

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import "./App.css";
 import Home from "./pages/Home.jsx"
 import Layout from "./components/Layout.jsx";
 import Category from "./pages/Category.jsx";
+import NotFound from "./pages/NotFound.jsx";
 
 function App() {
   return (
@@ -10,6 +11,7 @@ function App() {
       <Route element={<Layout />}>
         <Route path="/" element={<Home />}/>
         <Route path="category/:categorie" element={<Category />} />
+        <Route path="*" element={<NotFound />} />
       </Route>
     </Routes>
   );
