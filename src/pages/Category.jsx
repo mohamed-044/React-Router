9ff100815151e30decd166bbdeffff626f34b3c4
mohamed-044 <mohamed.boulatika04@gmail.com>
2025-07-21 17:21:28 +0200

    add useNavigate with bootstrap button to redirect on homepage

--- src/pages/Category.jsx
+++ src/pages/Category.jsx
@@ -1,9 +1,16 @@
 import { useParams } from "react-router";
+import { useNavigate } from "react-router";
+import { Button } from "react-bootstrap";
 const Category = () => {
   const { categorie } = useParams();
+  const navigate = useNavigate();
+      const handleLogin = () => {
+      navigate('/');
+      };
   return (
     <div>
       <h1>{categorie}</h1>
+      <Button className="my-3" onClick={handleLogin}>Revenir a la page d'accueil</Button>
     </div>
   );
 };
